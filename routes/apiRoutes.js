1b5811fcf0c117cfbed6dd466ada5ecdb44f80c0
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-22 17:56:58 -0600

    working on note saving functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,36 @@
-// requiring in the noteData file
-let noteData = require('../data/noteData');
+// requiring in modules needed
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function (app) {
 	// handling get requests for /api/notes
-	app.get('/api/notes', function (req, res) {
-		res.json(noteData);
+	app.get('/api/notes', (req, res) => {
+		let noteFile = path.join(__dirname, '../db/db.json');
+		fs.readFile(noteFile, 'utf8', (error, data) => {
+			if (error) {
+				console.error(error);
+			}
+			res.json(JSON.parse(data));
+		});
 	});
 
 	// handling post requests for /api/notes
 	app.post('/api/notes', function (req, res) {
-		noteData.push(req.body);
-		res.json(true);
+		let newNote = req.body;
+		console.log(newNote);
+		let noteFile = path.join(__dirname, '../db/db.json');
+		fs.readFile(noteFile, 'utf8', (error, data) => {
+			if (error) {
+				console.error(error);
+			}
+			let currentNotes = JSON.parse(data);
+
+			currentNotes.push(newNote);
+
+			fs.writeFile(noteFile, JSON.stringify(currentNotes), (err) =>
+				err ? console.error(err) : console.log('Note added!')
+			);
+			res.json(currentNotes);
+		});
 	});
 };
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ let PORT = process.env.PORT || 8080;
 // setting up express app for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/db'));
 
 // link to routes files
 require('./routes/htmlRoutes')(app);
