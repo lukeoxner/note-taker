83ed786d1d2755d460d6a5e2992463fe4fd4a994
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-22 16:42:40 -0600

    work on html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,5 +2,13 @@
 const path = require('path');
 
 module.exports = function (app) {
-	app.get('/');
+	// set up route for notes page
+	app.get('/notes', function (req, res) {
+		res.sendFile(path.join(__dirname, '../public/notes.html'));
+	});
+
+	// set up default route to home page
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, '../public/index.html'));
+	});
 };
