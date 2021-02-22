734ca8f57832c552626ff44b525873c0adce67e0
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-22 16:15:46 -0600

    set up ports and middleware

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-var express = require('express');
-var path = require('path');
+// requiring in necessary node modules
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-var app = express();
+// telling node i am creating an express server
+const app = express();
+
+// setting port for heroku deployment, or localhost
+let PORT = process.env.PORT || 8080;
+
+// setting up express app for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
